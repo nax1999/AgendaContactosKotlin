c40b2622ef2f04e6a572c454f4ef885964dd5e07
Ignacio Vinuela Herrero <57708826+nax1999@users.noreply.github.com>
2021-02-16 00:01:58 +0100

    Agenda

--- Contactos/app/src/main/java/com/example/contactos/MainActivity.kt
+++ Contactos/app/src/main/java/com/example/contactos/MainActivity.kt
@@ -1,18 +1,50 @@
 package com.example.contactos
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission_group.CAMERA
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.media.MediaRecorder.VideoSource.CAMERA
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+
 class MainActivity : AppCompatActivity() {
+    private val cameraRequest = 1888
+    lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), cameraRequest)
+        imageView = findViewById(R.id.imageView)
+        val photoButton: Button = findViewById(R.id.button)
+        photoButton.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, cameraRequest)
+        }
     }
+    fun onActivityResult1(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == cameraRequest) {
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
+        }
+    }
+
     fun addContact(view: View) {
         val Nombre: EditText = findViewById(R.id.idNombre)
         val Numero: EditText = findViewById(R.id.idTelefono)
